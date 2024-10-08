d9a6419a8904e6901e1446b83fdf995c297ce15d
shklx <amirrostami.b1994@gmail.com>
2024-10-08 14:16:34 +0330

    initial Develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cardImages = [
+  { src: '/pics/j1.jpeg', matched: false },
+  { src: '/pics/j2.jpeg', matched: false },
+  { src: '/pics/j3.jpeg', matched: false },
+  { src: '/pics/j4.jpeg', matched: false },
+  { src: '/pics/j5.jpeg', matched: false },
+  { src: '/pics/j6.jpeg', matched: false },
+]
+
+interface CardsLike {
+  src: string,
+  id: number,
+  matched: boolean
 }
 
-export default App;
+export default function App() {
+
+  if (window.localStorage.getItem('record') === null) {
+    window.localStorage.setItem('record', '0');
+  }
+
+  const [card, setCards] = useState<CardsLike[]>([]);
+  const [turn, setTurn] = useState<number>(0);
+  const [ch1, setCh1] = useState<CardsLike | null>(null);
+  const [ch2, setCh2] = useState<CardsLike | null>(null);
+  const [disable, setDisable] = useState(true)
+
+  // all useEffects : 
+
+  useEffect(() => {
+    cardShuffle()
+  }, [])
+
+  useEffect(() => {
+    if (ch1 && ch2) {
+      setDisable(false)
+      if (ch2.src === ch1.src) {
+        setCards(prevCard => (
+          prevCard.map(card => {
+            if (card.src === ch1.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          })
+        ))
+        resIncrese()
+      } else {
+        setTimeout(() => resIncrese(), 1000);
+      }
+    }
+  }, [ch1, ch2])
+
+
+  return (
+    <div className='App'>
+      <h1>حافظه تصویری خودتو بسنج!</h1>
+      <button onClick={cardShuffle}>بازی جدید</button>
+      <span>تا الان -{turn}- بار تلاش کردی</span>
+
+      <div className='card-grid'>
+        {
+          card.map(card =>
+          (<Card
+            key={card.id}
+            imgSrc={card}
+            handleCover={handleChoice}
+            rotate={card.matched || ch1 === card || ch2 === card}
+          />))
+        }
+      </div>
+    </div>
+  )
+
+  // Functions :
+
+  function cardShuffle() {
+    const shCards = [...cardImages, ...cardImages];
+
+    for (let i = shCards.length - 1; i >= 0; i--) { // use knuth-shuffle for shuffle cardds
+      const j = Math.floor(Math.random() * (i + 1));
+      [shCards[i], shCards[j]] = [shCards[j], shCards[i]];
+    }
+    const addedId = shCards.map(item => ({ ...item, id: Math.random() }));
+
+    setCh1(null);
+    setCh2(null);
+    setTurn(0);
+    setCards(addedId);
+  }
+
+  // reset choices and increase turn
+  function resIncrese() {
+    setCh1(null);
+    setCh2(null);
+    setTurn(prevTurn => ++prevTurn);
+    setDisable(true);
+  }
+
+  function handleChoice(card: CardsLike) {
+    if (disable) {
+      ch1 ? setCh2(card) : setCh1(card);
+    }
+  }
+}
---
+++ src/components/Card.tsx
@@ -0,0 +1,26 @@
+import './Card.css'
+
+interface CardsLike {
+  src: string,
+  id: number,
+  matched: boolean
+}
+
+interface CardProps {
+  imgSrc: CardsLike,
+  handleCover(src: CardsLike): void,
+  rotate: boolean,
+}
+
+export default function Card({ imgSrc, handleCover, rotate }: CardProps) {
+  return (
+    <div className='grid-item'>
+      <img className='main-img' src={imgSrc.src} alt="no-image" />
+      <img
+        className={`cover-img ${rotate ? 'rotate' : ''} `}
+        onClick={() => { handleCover(imgSrc) }}
+        src='/pics/cover4.jpeg'
+        alt="no-image" />
+    </div>
+  )
+}
